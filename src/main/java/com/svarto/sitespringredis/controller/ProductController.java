44628982e93478c4eb5dca85c3164cada1453412
Ilya <ilyakononovv@mail.ru>
2024-05-03 10:48:58 +0400

    сделал сохранения откликов с страницы с товаром

--- src/main/java/com/svarto/sitespringredis/controller/ProductController.java
+++ src/main/java/com/svarto/sitespringredis/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.svarto.sitespringredis.controller;
 
 import com.svarto.sitespringredis.Category;
 import com.svarto.sitespringredis.Product;
+import com.svarto.sitespringredis.Response;
 import com.svarto.sitespringredis.User;
+import com.svarto.sitespringredis.repos.ResponseRepository;
 import com.svarto.sitespringredis.services.CategoryService;
 import com.svarto.sitespringredis.services.ProductService;
 import com.svarto.sitespringredis.services.ResponseService;
@@ -16,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.time.ZonedDateTime;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -32,9 +35,12 @@ public class ProductController {
     @Autowired
     private ResponseService responseService;
 
+    @Autowired
+    private final ResponseRepository responseRepository;
+
     @GetMapping("/")
     public String products(@RequestParam(name = "searchWord", required = false) String title, Principal principal,
-                           Long cat_id, Model model) {
+            Long cat_id, Model model) {
         model.addAttribute("products", productService.list(title));
         model.addAttribute("categories", categoryService.list());
         model.addAttribute("user", productService.getUserByPrincipal(principal));
@@ -46,18 +52,18 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public String productInfo(@PathVariable Long id, Model model, Principal principal) {
         Product product = productService.getProductById(id);
-        Optional <User> optionalUser = (Optional<User>) userService.getUserByUser_id(product.getUser_id());
-        Optional<Category> optionalCategory = (Optional<Category>) categoryService.getCategoryById(Long.valueOf(product.getCategory_id()));
+        Optional<User> optionalUser = (Optional<User>) userService.getUserByUser_id(product.getUser_id());
+        Optional<Category> optionalCategory = (Optional<Category>) categoryService
+                .getCategoryById(Long.valueOf(product.getCategory_id()));
 
-        if(optionalCategory.isPresent()){
+        if (optionalCategory.isPresent()) {
             Category category = optionalCategory.get();
             User user = optionalUser.get();
             model.addAttribute("category", category);
             model.addAttribute("main_user", user);
-        }
-        else{
+        } else {
             // Обработка случая, когда категория была не указана
-            Category category = new Category((long) -1, "не найдено" );
+            Category category = new Category((long) -1, "не найдено");
             model.addAttribute("category", category);
         }
 
@@ -67,6 +73,20 @@ public class ProductController {
         return "index_info";
     }
 
+    @PostMapping("/product/{id}")
+    public String createResponse(@PathVariable Long id, @RequestParam("message") String message, Principal principal) {
+        User user = productService.getUserByPrincipal(principal);
+        Response response = new Response();
+        response.setMessage(message);
+        response.setCustomerId(user.getId());
+        response.setProductId(id);
+        response.setMessage_date(ZonedDateTime.now());
+        System.out.println("Making response with message" + message);
+        responseRepository.save(response);
+        System.out.println(responseRepository.findAll());
+        return "redirect:/product/{id}";
+    }
+
     public String makeResponse(@PathVariable("id") Long id, Principal principal) {
         Product product = productService.getProductById(id);
         String message = "bad idea";
@@ -74,34 +94,35 @@ public class ProductController {
         responseService.makeResponse(message, principal, product);
         return "index_info";
     }
+
     @PostMapping("/product/create")
-public String createProduct(@RequestParam("title") String title,
-                            @RequestParam("price") int price,
-                            @RequestParam("city") String city,
-                            @RequestParam("description") String description,
-                            @RequestParam("image") MultipartFile image,
-                            @RequestParam("Category_id") int  category_id,
-                            Principal principal) {
-    Product product = new Product();
-    product.setTitle(title);
-    product.setPrice(price);
-    product.setCity(city);
-    product.setDescription(description);
-    product.setCategory_id(category_id);
+    public String createProduct(@RequestParam("title") String title,
+            @RequestParam("price") int price,
+            @RequestParam("city") String city,
+            @RequestParam("description") String description,
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("Category_id") int category_id,
+            Principal principal) {
+        Product product = new Product();
+        product.setTitle(title);
+        product.setPrice(price);
+        product.setCity(city);
+        product.setDescription(description);
+        product.setCategory_id(category_id);
 
-    if (!image.isEmpty()) {
-        try {
-            String imageBase64 = Base64.getEncoder().encodeToString(image.getBytes());
-            product.setImage(imageBase64);
-        } catch (IOException e) {
-            System.out.println("Error processing image");
-            e.printStackTrace();
+        if (!image.isEmpty()) {
+            try {
+                String imageBase64 = Base64.getEncoder().encodeToString(image.getBytes());
+                product.setImage(imageBase64);
+            } catch (IOException e) {
+                System.out.println("Error processing image");
+                e.printStackTrace();
+            }
         }
-    }
 
-    productService.saveProduct(principal, product);
-    return "redirect:/";
-}
+        productService.saveProduct(principal, product);
+        return "redirect:/";
+    }
 
     @PostMapping("/product/delete/{id}")
     public String deleteProduct(@PathVariable Long id) {
@@ -114,6 +135,7 @@ public String createProduct(@RequestParam("title") String title,
         productService.deleteProduct(id);
         return "redirect:/user/{user_id}";
     }
+
     @GetMapping("/my/products")
     public String userProducts(Principal principal, Model model) {
         User user = productService.getUserByPrincipal(principal);
